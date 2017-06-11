ed678391a36f69ffa3c4f67e3a3dd2dd2ab6d7dc
WarriusBird <nbgibson@crimson.ua.edu>
2017-06-11 16:55:24 -0400

    foo

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject.DontDestroyOnLoad();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
