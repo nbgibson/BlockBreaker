f1e241e93f8f4b62039a4e2fe57a58adcc204dd6
WarriusBird <nbgibson@crimson.ua.edu>
2017-06-18 23:45:16 -0400

    1st pass ball physics and control
    Added physics to the ball and some pre-game starting conditions to control it.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+    public Paddle paddle;
+    private bool hasStarted = false;
+    private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+        paddleToBallVector = this.transform.position - paddle.transform.position;
+        print(paddleToBallVector);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hasStarted)
+        {
+            this.transform.position = paddle.transform.position + paddleToBallVector;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                hasStarted = true;
+                print("Mouse clicked");
+
+                this.rigidbody2D.velocity = new Vector2(2f, 10f);
+
+            }
+        }
+        
+	}
+}
---
+++ Assets/Scripts/Lose_Collider.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lose_Collider : MonoBehaviour {
+
+    public LevelManager levelManager;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print("Trigger");
+        levelManager.LoadLevel("Win");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+    }
+
+    
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
