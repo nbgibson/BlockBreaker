2847d0e61577e4671a62de578d3e984e6ee539e8
WarriusBird <nbgibson@crimson.ua.edu>
2017-06-22 00:27:13 -0400

    Block Damage
    Consolidated sprites into a spritesheet and created additional sprites to demonstrate damage done to multihit blocks. Implemented damage tracking system as well as a method to load in new sprites to help visualize damage.

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -4,14 +4,13 @@ using System.Collections;
 public class Brick : MonoBehaviour {
 
     public int maxHits;
+    public Sprite[] hitSprites;
 
     private int timesHit;
-    private LevelManager levelManager;
 
 	// Use this for initialization
 	void Start () {
         timesHit = 0;
-        levelManager = GameObject.FindObjectOfType<LevelManager>();
 	}
 	
 	// Update is called once per frame
@@ -26,5 +25,15 @@ public class Brick : MonoBehaviour {
         {
             Destroy(gameObject);
         }
+        else
+        {
+            LoadSprites();
+        }
+    }
+
+    void LoadSprites()
+    {
+        int spriteIndex = timesHit - 1;
+        this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
     }
 }
