69c78dbe29c41f3f441d341ceaeb4edfba92d4b1
nbgibson <nbgibson@crimson.ua.edu>
2017-09-07 13:00:08 -0400

    Commented out lines I meant to comment out before regarding non-Windows builds

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,21 +1,21 @@
 #! /bin/sh
 
 # NOTE the command args below make the assumption that your Unity project folder is
-#  a subdirectory of the repo root directory, e.g. for this repo "unity-ci-test" 
-#  the project folder is "UnityProject". If this is not true then adjust the 
+#  a subdirectory of the repo root directory, e.g. for this repo "unity-ci-test"
+#  the project folder is "UnityProject". If this is not true then adjust the
 #  -projectPath argument to point to the right location.
 
 ## Run the editor unit tests
 #echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
 #/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-nographics \
-	-silent-crashes \
-	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-runEditorTests \
-	-editorTestsResultFile $(pwd)/test.xml \
-	-quit
+#	-batchmode \
+#	-nographics \
+#	-silent-crashes \
+#	-logFile $(pwd)/unity.log \
+#	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
+#	-runEditorTests \
+#	-editorTestsResultFile $(pwd)/test.xml \
+#	-quit
 
 #rc0=$?
 #echo "Unit test logs"
@@ -41,13 +41,13 @@ cat $(pwd)/unity.log
 
 #echo "Attempting build of ${UNITYCI_PROJECT_NAME} for Web"
 #/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-nographics \
-	-silent-crashes \
-	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-buildOSXUniversalPlayer "$(pwd)/Build/osx/${UNITYCI_PROJECT_NAME}.app" \
-	-quit
+#	-batchmode \
+#	-nographics \
+#	-silent-crashes \
+#	-logFile $(pwd)/unity.log \
+#	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
+#	-buildOSXUniversalPlayer "$(pwd)/Build/osx/${UNITYCI_PROJECT_NAME}.app" \
+#	-quit
 
 #rc2=$?
 #echo "Build logs (OSX)"
